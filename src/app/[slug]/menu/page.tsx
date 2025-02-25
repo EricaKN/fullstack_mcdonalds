10b788d43432f4a57adc12bc10a27a22638dd8e7
ericakn <erica.k.nishimura@gmail.com>
2025-02-25 11:29:56 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,36 @@
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod);
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant}></RestaurantHeader>
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
